5dc06e18d24506bdb56adab9c494b72ed57c21f8
Jonathan Van Schenck <jvschenck@novadynamics.com>
2022-07-27 08:01:55 -0700

    Add module exports

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
 
-// module.exports=exports={
-//     app : require("./lib/ROSApp.js"),
-//     // client : require("./lib/ROSClient.js"),
-// };
-module.exports=exports=require("./lib/ROSApp.js");
+// Load the App as the default export
+const App = require("./lib/ROSApp.js");
+
+// Attach contructors for `const { ROSClient } = require('rest-over-sockets');` style requiring
+App.ROSApp = require("./lib/ROSApp.js");
+App.ROSClient = require("./lib/ROSClient.js");
+App.ROSRequest = require("./lib/ROSRequest.js");
+App.ROSResponse = require("./lib/ROSResponse.js");
+
+module.exports=exports=App;
